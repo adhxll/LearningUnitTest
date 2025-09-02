af3d18bbb27511974435a5e7faf964ddd49bf229
Adhella Subalie <adhellasbl@gmail.com>
2025-09-02 12:43:20 +0800

    Initial Commit

---
+++ LearningUnitTest/LearningUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningUnitTestApp.swift
+//  LearningUnitTest
+//
+//  Created by Adhella Subalie on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
